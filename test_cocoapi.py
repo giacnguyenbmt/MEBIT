8d73c11f64653ea3cb1242d9a9c4dec68fb9d0fe
Nguyen Pham <phamduygiacnguyen9c@gmail.com>
2022-09-06 17:03:58 +0700

    Update code

---
+++ test_cocoapi.py
@@ -0,0 +1,14 @@
+from pycocotools.coco import COCO
+import cv2
+import utils.rrc_evaluation_funcs_1_1 as rrc_evaluation_funcs
+
+def create_coco_dataset(img, points_list, transcriptions_list):
+    data = {}
+    
+
+
+if __name__ == "__main__":
+    img = 'data/img_16.jpg'
+    points_list = []
+    transcriptions_list = []
+    create_coco_dataset(img, points_list, transcriptions_list)
